c002e2ec7c5330ec66c2b8c71bb841e05583233e
corry257 <145715803+corry257@users.noreply.github.com>
2025-04-03 22:23:01 -0300

    upload de repositório - aula sobre js

---
+++ aula-js-01/concatenar.js
@@ -0,0 +1,8 @@
+let palavra01 = "quero";
+let palavra02 = "um";
+let palavra03 = "switch 2!";
+
+console.log(palavra01, palavra02, palavra03);
+console.log(palavra01 + palavra02 + palavra03);
+console.log(`${palavra01} ${palavra02} ${palavra03}♥
+            \n ♥♥♥♥♥`);
---
+++ aula-js-01/ola.js
@@ -0,0 +1,16 @@
+console.log("olá mundo!");
+
+var msg = "Eu quero um nintendo switch 2!";
+console.log(msg);
+
+let resposta  = "meus alunos são lindos! #SW2";
+console.log(resposta);
+
+const turma = "eu ♥ o 2DS";
+console.log(turma);
+
+resposta = "me comprem um switch 2, por favor?";
+console.log(resposta);
+
+// turma = "teste";    variaveis do tipo constante não podem receber dois valores em linhas diferentes
+// console.log(turma);
