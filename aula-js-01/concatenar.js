a424afdaa483f9fb96297ad14b9d084cd3d28044
corry257 <145715803+corry257@users.noreply.github.com>
2025-05-08 20:19:45 -0300

    Delete aula-js-01 directory

--- aula-js-01/concatenar.js
+++
@@ -1,8 +0,0 @@
-let palavra01 = "quero";
-let palavra02 = "um";
-let palavra03 = "switch 2!";
-
-console.log(palavra01, palavra02, palavra03);
-console.log(palavra01 + palavra02 + palavra03);
-console.log(`${palavra01} ${palavra02} ${palavra03}♥
-            \n ♥♥♥♥♥`);
